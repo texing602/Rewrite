a8144cf88a6dcab5d3d680f69b4c3371e984b34a
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-06-02 22:44:07 +0800

    Update ruleMatchingTime.js

--- ruleMatchingTime.js
+++ ruleMatchingTime.js
@@ -6,6 +6,7 @@ let initial = {
     numberOfRequests: 0,
     totalMatchingTime: 0,
     latestRecordTime: 0,
+    longestMatchingTime: 0,
 };
 
 let history = JSON.parse(
@@ -28,8 +29,12 @@ $httpAPI("GET", "/v1/requests/recent", null, (body) => {
             request.setupCompletedDate > history.latestRecordTime &&
             request.timingRecords[0].name == "Rule Evaluating"
         ) {
-            history.totalMatchingTime += request.timingRecords[0].duration;
+            let matchingTime = request.timingRecords[0].duration;
+            history.totalMatchingTime += matchingTime;
             history.numberOfRequests += 1;
+            if (matchingTime > history.longestMatchingTime) {
+                history.longestMatchingTime = matchingTime;
+            }
         }
     });
 
@@ -42,7 +47,7 @@ $httpAPI("GET", "/v1/requests/recent", null, (body) => {
 
     $done({
         title: "Rule Matching Time",
-        content: `统计数量: ${history.numberOfRequests}\n平均时间: ${avgMatchingTime} ms\n总匹配时间: ${(history.totalMatchingTime * 1000).toFixed(2)} ms\n获取时间: ${new Date().toLocaleString()}`,
+        content: `统计数量: ${history.numberOfRequests}\n平均时间: ${avgMatchingTime} ms\n总匹配时间: ${(history.totalMatchingTime * 1000).toFixed(2)} ms\n最长匹配时间: ${(history.longestMatchingTime * 1000).toFixed(2)} ms\n获取时间: ${new Date().toLocaleString()}`,
         icon: "bolt.horizontal.circle.fill",
     });
 });
