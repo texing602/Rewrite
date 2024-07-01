b715677169895064a632db6cbd8d9ee9daa29122
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-07-01 21:22:18 +0800

    Update MJGS.js

--- lsp/MJGS.js
+++ lsp/MJGS.js
@@ -5,7 +5,7 @@ https:\/\/(mfjk|gnjk|buyaoda).+\.com\/(playerinfo|api\/get_advert|api\/home\/pop
 #游戏
 ^https:\/\/(mfjk|gnjk|buyaoda).+\.com\/request_game\/v3\/game\/list url reject-dict
 #视频
-https?:\/\/(mfjk|gnjk|buyaoda).+\.com\/api\/(comic|video|short_movie_info|novel|gallery) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgshd.js
+https?:\/\/(mfjk|gnjk|buyaoda).+\.com\/api\/(comic|video|short_movie_info|novel|gallery|audio) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgshd.js
 
 [mitm]
 hostname = gnjk.eapep.com, mfjk.eapep.com, buyaoda.*.com
