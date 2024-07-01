0a7e80e7b9c081ba34ed6f1fc465b42dc311a03d
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-07-01 21:01:16 +0800

    Update MJGS.js

--- lsp/MJGS.js
+++ lsp/MJGS.js
@@ -3,7 +3,7 @@
 #广告
 https:\/\/(mfjk|gnjk|buyaoda).+\.com\/(playerinfo|api\/get_advert|api\/home\/popup) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgsad.js
 #游戏
-^https:\/\/gnjk\.eapep\.com\/request_game\/v3\/game\/list url reject-dict
+^https:\/\/(mfjk|gnjk|buyaoda).+\.com\/request_game\/v3\/game\/list url reject-dict
 #视频
 https?:\/\/(mfjk|gnjk|buyaoda).+\.com\/api\/(comic|video|short_movie_info|novel|gallery) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/mjgshd.js
 
